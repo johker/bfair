21dc0b0d91ec5f498583416bd64fe0a79c759c40
unknown <kern@Imitator.virtual7.local>
2014-03-26 20:20:36 +0100

    Fixed test env IP

--- config/env/test.js
+++ config/env/test.js
@@ -2,5 +2,5 @@
  * Vagrant / Puppet VM 
  */
 module.exports = {
-    db: 'mongodb://127.192.33.10:27018/be-fair-authentication'      
+    db: 'mongodb://192.168.33.10:27018/be-fair-authentication'      
 }
