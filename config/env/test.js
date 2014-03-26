db2b62c2f138e1072dae5ce30a838c8cb6f22859
unknown <kern@Imitator.virtual7.local>
2014-03-26 17:14:29 +0100

    Test environment port changed

--- config/env/test.js
+++ config/env/test.js
@@ -2,5 +2,5 @@
  * Vagrant / Puppet VM 
  */
 module.exports = {
-    db: 'mongodb://127.192.33.10:27017/be-fair-authentication'      
+    db: 'mongodb://127.192.33.10:27018/be-fair-authentication'      
 }
