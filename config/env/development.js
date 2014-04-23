1dda15d8add671444b538ee43dc5fff35565dedd
unknown <kern@Imitator.virtual7.local>
2014-04-23 08:35:29 +0200

    Changed logdir config

--- config/env/development.js
+++ config/env/development.js
@@ -2,5 +2,8 @@
  * local properties
  */
 module.exports = {
-    db: 'mongodb://127.0.0.1:27017/be-fair-authentication'      
+    db: 'mongodb://127.0.0.1:27017/be-fair-authentication',      
+    logs: {
+       dir: '/var/log/bfair/'
+    }
 }
--- config/env/production.js
+++ config/env/production.js
@@ -2,5 +2,8 @@
  * EC2 specific properties
  */
 module.exports = { 
-	db: 'mongodb://localhost:27017/be-fair-authentication'
+	db: 'mongodb://localhost:27017/be-fair-authentication',
+	logs: {
+	   dir: '/var/log/bfair/'
+	}
 }
